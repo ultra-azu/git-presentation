09c1ee27a099c01a1e262d1c0904404cf6ef2ac3
Ultra <ultra.public@proton.me>
2026-02-12 11:12:33 -0400

    Add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,35 @@
+def fizz_buzz(n):
+    result = []
+
+    for i in range(1, n + 1):
+
+        # Check if i is divisible by both 2 and 3
+        if i % 2 == 0 and i % 3 == 0:
+
+            # Add "FizzBuzz" to the result list
+            result.append("FizzBuzz")
+
+        # Check if i is divisible by 2
+        elif i % 2 == 0:
+
+            # Add "Fizz" to the result list
+            result.append("Fizz")
+
+        # Check if i is divisible by 3
+        elif i % 3 == 0:
+
+            # Add "Buzz" to the result list
+            result.append("Buzz")
+        else:
+
+            # Add the current number as a string to the
+            # result list
+            result.append(str(i))
+
+    return result
+
+
+# Driver code
+n = 10
+result = fizz_buzz(n)
+print(' '.join(result))
