560ad28022fad3ffb4cfb868aac9babda5fe3209
Ultra <ultra.public@proton.me>
2026-03-17 07:59:13 -0400

    Change to 4 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,19 +4,19 @@ def fizz_buzz(n):
     for i in range(1, n + 1):
 
         # Check if i is divisible by both 2 and 3
-        if i % 2 == 0 and i % 3 == 0:
+        if i % 4 == 0 and i % 5 == 0:
 
             # Add "HolaMundo" to the result list
             result.append("HolaMundo")
 
         # Check if i is divisible by 2
-        elif i % 2 == 0:
+        elif i % 4 == 0:
 
             # Add "Hola" to the result list
             result.append("Hola")
 
         # Check if i is divisible by 3
-        elif i % 3 == 0:
+        elif i % 5 == 0:
 
             # Add "Mundo" to the result list
             result.append("Mundo")
